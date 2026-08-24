27b088a41dfd1e3916827b65fb94cc663485671e
Vidyashri Kajagar <vidyashrikajagar@gmail.com>
2026-08-24 21:26:56 +0530

    fix databasr config import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const db = require("./config/db");
+const db = require("./config/db.js");
 
 const authRoutes = require("./routes/authRoutes");
 const lostRoutes = require("./routes/lostRoutes");
