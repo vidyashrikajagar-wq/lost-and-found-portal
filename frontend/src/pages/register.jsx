a5e1ccc07d357b4ec703bd9d70835620d115d141
Vidyashri Kajagar <vidyashrikajagar@gmail.com>
2026-08-26 22:07:50 +0530

    fix deployed api urls

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -24,7 +24,7 @@ function Register() {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/auth/register",
+        "api/auth/register",
         {
           method: "POST",
           headers: {
