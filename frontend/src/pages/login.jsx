77372e31861204b382e646d6710f58c4027fdb4e
Vidyashri Kajagar <vidyashrikajagar@gmail.com>
2026-08-26 22:54:54 +0530

    fix frontend  api urls

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -23,7 +23,7 @@ function Login() {
 
     try {
 
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
